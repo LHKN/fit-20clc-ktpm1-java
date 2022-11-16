f71738d68dc5438e4639c07fc0e8c67aada950e9
LHKN <79393909+LHKN@users.noreply.github.com>
2022-11-16 18:54:55 +0700

    feat: add method to view sorted dict on console

--- PA_Dict_20127679/slangDict.java
+++ PA_Dict_20127679/slangDict.java
@@ -5,16 +5,46 @@ import java.io.*;
 //import java.util.stream.*;
 
 public class slangDict {
-    private static Scanner input = new Scanner(System.in);
-    private ArrayList<slangCard> dictionary = new ArrayList<slangCard>();
+    // private static Scanner input = new Scanner(System.in);
     
+    private HashMap<String,String[]> dictionary = new HashMap<>();
+    private HashMap<String,String[]> history = new HashMap<>();
+    //put defition as slangCard to make multiple duplicate slangs?
+
+    //input file dict
+    void inputDict(String filename){
+
+    }
+
+    //output file dict
+    void outputDict(String filename) throws IOException{
+        String s;
+        FileWriter fw;
+        try{
+            fw = new FileWriter(filename);
+        }
+        catch(IOException e){
+            System.out.println("Error opening file");
+            //swingUI
+            return;
+        }
+        for (String word:dictionary.keySet()){
+            s=word+"'"+dictionary.get(word)+"/n";
+            fw.write(s);
+        }
+        fw.close();
+        System.out.println("Output Successfully!");
+    }
+
+        //need to save history?
+
     //search slang word
 
     //search definition
 
     //show search history 
     
-    //add slang --> check condition
+    //add slang --> check condition: overwrite/duplicate
 
     //edit slang
 
@@ -27,5 +57,39 @@ public class slangDict {
     //minigame: 1 slang 4 definition options
 
     //minigame: 1 def 4 slang
+
+    //additional methods for support/debug
+    LinkedHashMap<String,String[]> sortedDict = new LinkedHashMap<>();
+    class wordComparator implements Comparator<String>{
+        public int compare(String s1, String s2){
+            return s1.compareTo(s2);
+        }
+    }
+
+    //sort dictionary
+    public void sortWord(){
+        wordComparator wc = new wordComparator();
+        ArrayList<String> wordList = new ArrayList<>();
+        for (String s: dictionary.keySet()) {
+            wordList.add(s);
+        }
+        Collections.sort(wordList,wc);
+
+        for (String s1: wordList) {
+            for (String s2: dictionary.keySet()) {
+                if (s1.equals(s2)) {
+                    sortedDict.put(s1, dictionary.get(s1));
+                }
+            }
+        }
+    }
+
+    //view dictionary on console
+    public void viewDictConsole(){
+        sortWord();
+        for (String word:sortedDict.keySet()){
+            System.out.println(word+"'"+sortedDict.get(word)+"/n");
+        }
+    }
 
 }
